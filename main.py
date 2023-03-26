8093d86c3d85327711037e93c4d5f776132056d1
Elphiie <92294372+Elphiie@users.noreply.github.com>
2023-03-26 16:50:08 +0200

    start from scratch

--- main.py
+++ main.py
@@ -275,8 +275,8 @@ def eval_genomes(genomes, config):
 
 
 def run_neat(config):
-    p = neat.Checkpointer.restore_checkpoint('neat-checkpoint-56')
-    # p = neat.Population(config)
+    # p = neat.Checkpointer.restore_checkpoint('neat-checkpoint-56')
+    p = neat.Population(config)
     p.add_reporter(neat.StdOutReporter(True))
     stats = neat.StatisticsReporter()
     p.add_reporter(stats)
