52e429b0d00b3dcd4cd0036a30d200b8624dbb38
Elphiie <92294372+Elphiie@users.noreply.github.com>
2023-03-27 22:43:39 +0200

    run from checkpoint 29

--- main.py
+++ main.py
@@ -275,13 +275,13 @@ def eval_genomes(genomes, config):
 
 
 def run_neat(config):
-    p = neat.Checkpointer.restore_checkpoint('neat-checkpoint-24')
+    p = neat.Checkpointer.restore_checkpoint('neat-checkpoint-29')
     # p = neat.Population(config)
     p.add_reporter(neat.StdOutReporter(True))
     stats = neat.StatisticsReporter()
     p.add_reporter(stats)
     p.add_reporter(neat.Checkpointer(1))
-    winner = p.run(eval_genomes, 25)
+    winner = p.run(eval_genomes, 10)
     with open("best.pickle", "wb") as f:
         pickle.dump(winner, f)
     
